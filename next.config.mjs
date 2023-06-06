c4e9c0e4a97d5d90ddff924799ea9a5625cfc6f7
iMidnight <mohaidarus@gmail.com>
2023-06-06 20:04:47 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,9 @@ const withMDX = nextMDX({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true
+  },
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
   experimental: {
